72daf3a2150eaa11a714e6dd4bd6f30f358a09b2
dzikri <dzikrimustaqim@gmail.com>
2025-10-20 22:55:29 +0700

    Initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS URLs when behind SSL proxy
+        if (request()->header('X-Forwarded-Proto') === 'https') {
+            \URL::forceScheme('https');
+        }
     }
 }
